fc400f430f81711c717462dbc33a63265dc97eff
Hiroyuki OYAMA <oyama@module.jp>
2025-04-21 23:48:10 +0900

    Remove unnecessary code blocks

--- src/bootsel_button.c
+++ src/bootsel_button.c
@@ -7,23 +7,6 @@
 #include <hardware/structs/ioqspi.h>
 #include <hardware/sync.h>
 
-/*
-bool __no_inline_not_in_flash_func(bb_get_bootsel_button)() {
-    const uint CS_PIN_INDEX = 1;
-    uint32_t flags = save_and_disable_interrupts();
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    for (volatile int i = 0; i < 1000; ++i);
-    bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    restore_interrupts(flags);
-    return button_state;
-}
-*/
-
 #define BUTTON_DEBOUNCE_MAX   10
 
 bool __no_inline_not_in_flash_func(bb_get_bootsel_button)() {
