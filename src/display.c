b078d1f99f8d40f6ee89109ae83505fd22a631cd
Hiroyuki OYAMA <oyama@module.jp>
2025-04-28 13:46:33 +0900

    Reduced display items

--- src/display.c
+++ src/display.c
@@ -15,16 +15,16 @@
 #include "ble_midi.h"
 #include "looper.h"
 
-#define ANSI_RESET           "\x1b[0m"
-#define ANSI_BRIGHT_RED      "\x1b[91m"
-#define ANSI_BRIGHT_GREEN    "\x1b[92m"
-#define ANSI_BRIGHT_BLUE     "\x1b[94m"
-#define ANSI_BRIGHT_MAGENTA  "\x1b[95m"
-#define ANSI_BRIGHT_CYAN     "\x1b[96m"
+#define ANSI_RESET "\x1b[0m"
+#define ANSI_BRIGHT_RED "\x1b[91m"
+#define ANSI_BRIGHT_GREEN "\x1b[92m"
+#define ANSI_BRIGHT_BLUE "\x1b[94m"
+#define ANSI_BRIGHT_MAGENTA "\x1b[95m"
+#define ANSI_BRIGHT_CYAN "\x1b[96m"
 
-#define ANSI_BOLD            "\x1b[1m"
-#define ANSI_FG_WHITE        "\x1b[97m"
-#define ANSI_BG_STEP_HL      "\x1b[105m"
+#define ANSI_BOLD "\x1b[1m"
+#define ANSI_FG_WHITE "\x1b[97m"
+#define ANSI_BG_STEP_HL "\x1b[105m"
 
 // Prints a single track row with step highlighting and note indicators.
 static void print_track(const char *label, const bool *steps, uint8_t current_step,
@@ -45,8 +45,8 @@ static void print_track(const char *label, const bool *steps, uint8_t current_st
 // Displays the looper's playback state, connection status, and track patterns.
 void display_update_looper_status(bool ble_connected, const looper_status_t *looper,
                                   const track_t *tracks, size_t num_tracks) {
-    size_t n = printf("[%s]", ble_connected ? ANSI_BRIGHT_GREEN "BLE-MIDI CONNECTED" ANSI_RESET: ANSI_BRIGHT_BLUE "BLE-MIDI WAITING" ANSI_RESET);
-    printf("%*s" ANSI_BOLD "#Pico_MIDI_Looper" ANSI_RESET "\n", 39 - n, " ");
+    printf(ANSI_BOLD "#Pico_MIDI_Looper" ANSI_RESET "\n");
+
     const char *state_label = ANSI_BRIGHT_BLUE "WAITING" ANSI_RESET;
     if (ble_connected) {
         switch (looper->state) {
@@ -64,8 +64,9 @@ void display_update_looper_status(bool ble_connected, const looper_status_t *loo
                 break;
         }
     }
+    printf("[%s] %s%u bpm\n" ANSI_RESET, state_label,
+           ((looper->current_step % LOOPER_CLICK_DIV) == 0 ? ANSI_BOLD : ""), looper->bpm);
 
-    printf("[%s] %s%u bpm" ANSI_RESET "\n", state_label, ((looper->current_step % LOOPER_CLICK_DIV) == 0 ? ANSI_BOLD : ""), looper->bpm);
     for (uint8_t i = 0; i < num_tracks; i++)
         print_track(tracks[i].name, tracks[i].pattern, looper->current_step,
                     i == looper->current_track);
